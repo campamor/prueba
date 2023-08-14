44252a1e5c19122816acc3b9e8e099588f2081f3
campamor <bego.guillermo.vila@gmail.com>
2023-08-14 18:59:32 +0200

    desarrollo2

--- prueba.php
+++ prueba.php
@@ -4,4 +4,4 @@ echo "Esto es una prueba";
 echo "Esto es una prueba";
 echo "Esto es una prueba";
 echo "estoy en desarrollo";
-echo "sigo en desarrollo";
+echo "sigo en desarrollo por picarenlamina";
